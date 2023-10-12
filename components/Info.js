ad896f413062075db039d9499a08f28aa1baf5e3
Dmytro Shpringer <shpringerd@gmail.com>
2023-10-12 20:27:31 +0300

    Update Info.js

--- components/Info.js
+++ components/Info.js
@@ -4,7 +4,7 @@ export default function Info() {
     return (
         <header>
             <img src="images/dmitro.jpg" className="my-img" alt="my photo" />
-            <h2>Dmitro Shpringer</h2>
+            <h2>Dmytro Shpringer</h2>
             <p>Frontend Developer</p>
             <a href="https://github.com/springdmitry?tab=repositories" target="_blank">github.com/springdmitry</a>
             <div className="links-container">
